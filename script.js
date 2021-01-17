e3e4f0a3bb3ad19331440e569254c5ee4070eddb
CarlSaqui29 <74756666+CarlSaqui29@users.noreply.github.com>
2021-01-17 13:54:58 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//all animations
+$(".section-2 .one").attr("uk-scrollspy", "cls: uk-animation-slide-left; target: img; delay: 500; repeat: false");
+$(".section-3 .row").attr("uk-scrollspy", "cls: uk-animation-fade; target: .img-fluid; delay: 100; repeat: false");
+
+
+
+//sidebar menu
 var ov_side = document.getElementById("overlay-side");
 
 function close_nav() {
@@ -18,4 +25,5 @@ $('.hamburger').click(function(){
 $('.sidebar ul li, .nav li').click(function(){
   $(this).addClass("active").siblings().removeClass("active");
 });
+
 
